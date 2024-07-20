f577ee92ba4d642cf945804dc44b98719ca9207e
Ishan Singh <ishan.1999@gmail.com>
2024-07-20 17:10:38 +0530

    added logic for bar chart and local storage usage

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const checkboxList = document.querySelectorAll('.checkbox');
-const cardContainer = document.querySelector('.checkbox-main');
 const inputs = document.querySelectorAll('input');
 const error = document.querySelector('.error');
-
-const tasks = {
+const barFill = document.querySelector('.bar-fill');
+const barText = document.querySelector('.bar-text');
+const tasks = JSON.parse(localStorage.getItem('tasks')) || {
   input1: {
     selected: false,
     content: '',
@@ -18,6 +18,52 @@ const tasks = {
   },
 };
 
+const prePopulate = () => {
+  if (localStorage.getItem('tasks')) {
+    inputs.forEach((input) => {
+      input.value = tasks[input.id]['content'];
+    });
+    checkboxList.forEach((checkbox) => {
+      if (tasks[checkbox.nextElementSibling.id]['selected']) {
+        checkbox.classList.add('selected');
+        checkbox.nextElementSibling.classList.add('completed');
+        checkbox.nextElementSibling.disabled = true;
+      }
+    });
+  }
+};
+
+prePopulate();
+
+const checkError = () => {
+  let completed = 0,
+    selected = 0;
+  for (const key of Object.keys(tasks)) {
+    if (tasks[key]['content']) {
+      completed++;
+    }
+  }
+  if (completed === 3) {
+    error.textContent = '';
+  } else {
+    error.textContent = 'Please select all 3 goals!';
+  }
+};
+checkError();
+
+const checkBarFill = () => {
+  let selected = 0;
+  for (const key of Object.keys(tasks)) {
+    if (tasks[key]['selected']) {
+      selected++;
+    }
+  }
+
+  barFill.style.width = `${(selected / 3) * 100}%`;
+  barText.textContent = `${selected}/3 Completed`;
+};
+checkBarFill();
+
 checkboxList.forEach((checkbox) => {
   checkbox.addEventListener('click', () => {
     if (checkbox.nextElementSibling.value.length > 0) {
@@ -25,6 +71,8 @@ checkboxList.forEach((checkbox) => {
       checkbox.nextElementSibling.classList.toggle('completed');
       tasks[checkbox.nextElementSibling.id]['selected'] =
         !tasks[checkbox.nextElementSibling.id]['selected'];
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      checkBarFill();
     }
 
     if (tasks[checkbox.nextElementSibling.id]['selected']) {
@@ -39,18 +87,8 @@ inputs.forEach((input) => {
   input.addEventListener('input', (e) => {
     if (!tasks[e.target.id]['selected']) {
       tasks[e.target.id]['content'] = e.target.value;
+      localStorage.setItem('tasks', JSON.stringify(tasks));
     }
-
-    let completed = true;
-    for (const key of Object.keys(tasks)) {
-      if (!tasks[key]['content']) {
-        completed = false;
-      }
-    }
-    if (completed) {
-      error.textContent = '';
-    } else {
-      error.textContent = 'Please select all 3 goals!';
-    }
+    checkError();
   });
 });
